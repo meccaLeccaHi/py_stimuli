628564e3c80619f96b0bb5686b2443a8a1f89bc2
adam <ajones173@gmail.com>
2016-12-19 15:26:45 -0600

    fixed trial abortion

--- expEyeTrack/fixCont.py
+++ expEyeTrack/fixCont.py
@@ -1,10 +1,14 @@
 '''
-Simple iohub eye tracker device demo. Shows how monitoring for central
-fixation monitoring could be done.
-No iohub config .yaml files are needed for this demo.
-Demo config is setup for an EyeLink(C) 1000 Desktop System. 
+Simple iohub eye tracker movie player. Presentation of movies is contingent on
+central fixation.
+No iohub config .yaml files are used.
+Setup for an EyeLink(C) 1000 Desktop System. 
 To to use a different eye tracker implementation, change the 
 iohub_tracker_class_path and eyetracker_config dict script variables.
+Currently loading raw (uncompressed) videos, 
+future versions should read compressed movies-
+    video: H.264 compressed,
+    audio: Linear PCM
 '''
 from psychopy import core, visual
 from psychopy.iohub.client import launchHubServer
@@ -13,20 +17,17 @@ import time, glob
 
 # Find movies matching wildcard search
 videopath = '/home/adam/Desktop/virtBox_share/JonesStimset/identity1/'
-videolist = glob.glob(videopath + '*100_audVid.avi')
+videolist = glob.glob(videopath + '*_audVid.avi')
 
-# Number if 'trials' to run
+# Number of trials of each stimulus to run
 BLOCK_REPS = 1
 TRIAL_COUNT = len(videolist) * BLOCK_REPS
-
-## Maximum trial time / time timeout
-#T_MAX = 10.0
 
 iohub_tracker_class_path = 'eyetracker.hw.sr_research.eyelink.EyeTracker'
 eyetracker_config = dict()
 eyetracker_config['name'] = 'tracker'
-eyetracker_config['model_name'] = 'EYELINK 1000 DESKTOP'
-#eyetracker_config['simulation_mode'] = True
+#eyetracker_config['model_name'] = 'EYELINK 1000 DESKTOP'
+eyetracker_config['simulation_mode'] = True
 eyetracker_config['runtime_settings'] = dict(sampling_rate=1000,
                                              track_eyes='RIGHT')
 
@@ -44,7 +45,8 @@ r = tracker.runSetupProcedure()
 win = visual.Window(display.getPixelResolution(),
                     units='pix',
                     fullscr=True,
-                    allowGUI=False
+                    allowGUI=False,
+                    color='black'
                     )
 
 gaze_ok_region = visual.Circle(win, radius=200, units='pix')
@@ -64,69 +66,72 @@ missing_gpos_str = 'Eye Position: MISSING. In Region: No\n'
 missing_gpos_str += 'Press space key to start next trial.'
 text_stim = visual.TextStim(win, text=text_stim_str,
                             pos=[0, int((-win.size[1]/2)*0.8)], height=24,
-                                 color='black',
+                                 color='white',
                                  alignHoriz='center',
                                  alignVert='center', 
                                  wrapWidth=win.size[0] * .9)
-
-# Run Trials.....
-for vidPath in videolist:
-
-    # Load movie from list
-    mov = visual.MovieStim3(win, vidPath, size=(366, 332),fps=30,
-                            flipVert=False, flipHoriz=False, loop=False) 
-                            
-    io.clearEvents()
-    tracker.setRecordingState(True)
-    run_trial = True
-    tstart_time = core.getTime()
-    
-    while run_trial is True:
-        
-        if mov.status != visual.FINISHED:
-            
-            # Get the latest gaze position in dispolay coord space..
-            gpos = tracker.getLastGazePosition()
-    
-            # Update stim based on gaze position
-            valid_gaze_pos = isinstance(gpos, (tuple, list))
-            gaze_in_region = valid_gaze_pos and gaze_ok_region.contains(gpos)
-            if valid_gaze_pos:
-                # If we have a gaze position from the tracker, update gc stim
-                # and text stim.
-                if gaze_in_region:
-                    gaze_in_region = 'Yes'
-                    mov.draw()
-                    fixation.draw()
-                    photodiode.draw()
-                else:
-                    gaze_in_region = 'No'
+# Run blocks.....
+for block in range(BLOCK_REPS):
                     
-                text_stim.text = text_stim_str % (gpos[0], gpos[1], gaze_in_region)
+    # Run Trials.....
+    for vidPath in videolist:
     
-                gaze_dot.setPos(gpos)
-            else:
-                # Otherwise just update text stim
-                text_stim.text = missing_gpos_str
+        # Create movie stim by loading movie from list
+        mov = visual.MovieStim3(win, vidPath, size=(366, 332),fps=30,
+                                flipVert=False, flipHoriz=False, loop=False) 
+                                
+        io.clearEvents()
+        tracker.setRecordingState(True)
+        run_trial = True
+        tstart_time = core.getTime()
+        
+        while run_trial is True:
+            
+            if mov.status != visual.FINISHED:
+                
+                # Get the latest gaze position in dispolay coord space..
+                gpos = tracker.getLastGazePosition()
+        
+                # Update stim based on gaze position
+                valid_gaze_pos = isinstance(gpos, (tuple, list))
+                gaze_in_region = valid_gaze_pos and gaze_ok_region.contains(gpos)
+                if valid_gaze_pos:
+                    # If we have a gaze position from the tracker, update gc stim
+                    # and text stim.
+                    if gaze_in_region:
+                        gaze_in_region = 'Yes'
+                        mov.draw()
+                        fixation.draw()
+                        photodiode.draw()
+                    else:
+                        gaze_in_region = 'No'
+                        mov.status = visual.FINISHED
+                        
+                    text_stim.text = text_stim_str % (gpos[0], gpos[1], gaze_in_region)
+        
+                    gaze_dot.setPos(gpos)
+                else:
+                    # Otherwise just update text stim
+                    text_stim.text = missing_gpos_str
+        
+                # Redraw stim
+                gaze_ok_region.draw()
+                text_stim.draw()
+                if valid_gaze_pos:
+                    gaze_dot.draw()
+        
+                # Display updated stim on screen.
+                flip_time = win.flip()
+        
+                # Check any new keyboard char events for a space key.
+                # If one is found, set the trial end variable.
+                #
+                if ' ' in keyboard.getPresses() or mov.status == visual.FINISHED:
+                    run_trial = False
     
-            # Redraw stim
-            gaze_ok_region.draw()
-            text_stim.draw()
-            if valid_gaze_pos:
-                gaze_dot.draw()
-    
-            # Display updated stim on screen.
-            flip_time = win.flip()
-    
-            # Check any new keyboard char events for a space key.
-            # If one is found, set the trial end variable.
-            #
-            if ' ' in keyboard.getPresses():
-                run_trial = False
-
-    # Current Trial is Done
-    # Stop eye data recording
-    tracker.setRecordingState(False)
+        # Current Trial is Done
+        # Stop eye data recording
+        tracker.setRecordingState(False)
 
 # All Trials are done
 # End experiment
