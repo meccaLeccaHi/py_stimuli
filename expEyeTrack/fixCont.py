41d269d67a2486e1bd05a6f3bd3a28908c951a5b
adam <ajones173@gmail.com>
2017-01-23 10:45:42 -0600

    added tracker-less mode

--- expEyeTrack/fixCont.py
+++ expEyeTrack/fixCont.py
@@ -18,7 +18,7 @@ import numpy as np
 
 # Find movies matching wildcard search
 videopath = '/home/adam/Desktop/virtBox_share/JonesStimset/identity1/'
-videolist = glob.glob(videopath + '*_audVid.avi')
+videolist = glob.glob(videopath + '*100_audVid.avi')
 
 # Number of trials of each stimulus to run
 BLOCK_REPS = 1
@@ -29,30 +29,39 @@ SCREEN_SIZE = np.array([1280, 1024])
 PHOTO_SIZE = 100
 PHOTO_POS = (SCREEN_SIZE - PHOTO_SIZE)/2 * [1, -1]
 
-SIMULATE = 1; # 1: yes, 2: no
+# toggle for presence of tracker
+EYE_TRACKER = 0; # 1: yes, 0: no
+# toggle to simulate tracker activity with mouse
+SIMULATE = 1; # 1: yes, 0: no
+    
+if EYE_TRACKER:
+    # Set-up tracker configuration dict
+    iohub_tracker_class_path = 'eyetracker.hw.sr_research.eyelink.EyeTracker'
+    eyetracker_config = dict()
+    eyetracker_config['name'] = 'tracker'
+    if SIMULATE:
+        eyetracker_config['simulation_mode'] = True
+    else:
+        eyetracker_config['model_name'] = 'EYELINK 1000 DESKTOP'
+        eyetracker_config['runtime_settings'] = dict(sampling_rate=1000, 
+            track_eyes='RIGHT')
 
-# Set-up tracker configuration dict
-iohub_tracker_class_path = 'eyetracker.hw.sr_research.eyelink.EyeTracker'
-eyetracker_config = dict()
-eyetracker_config['name'] = 'tracker'
-if SIMULATE:
-    eyetracker_config['simulation_mode'] = True
+    # Since no experiment or session code is given, no iohub hdf5 file
+    # will be saved, but device events are still available at runtime
+    io = launchHubServer(**{iohub_tracker_class_path: eyetracker_config})
+    # Get iohub device for tracker
+    tracker = io.devices.tracker
 else:
-    eyetracker_config['model_name'] = 'EYELINK 1000 DESKTOP'
-eyetracker_config['runtime_settings'] = dict(sampling_rate=1000,
-                                             track_eyes='RIGHT')
-
-# Since no experiment or session code is given, no iohub hdf5 file
-# will be saved, but device events are still available at runtime
-io = launchHubServer(**{iohub_tracker_class_path: eyetracker_config})
-
-# Get some iohub devices for future access
+    io = launchHubServer()
+    
+# Get iohub devices for future access
 keyboard = io.devices.keyboard
 display = io.devices.display
-tracker = io.devices.tracker
 
-# Run eyetracker calibration
-r = tracker.runSetupProcedure()
+if EYE_TRACKER:
+    # Run eyetracker calibration
+    r = tracker.runSetupProcedure()
+    
 win = visual.Window(SCREEN_SIZE.tolist(),
                     units='pix',
                     fullscr=True,
@@ -92,11 +101,10 @@ for block in range(BLOCK_REPS):
         # Create movie stim by loading movie from list
         mov = visual.MovieStim3(win, vidPath, size=(366, 332), fps = 30,
                                 flipVert=False, flipHoriz=False, loop=False) 
-        print('movie size=%s' % mov.size)
-        print('duration=%.2fs' % mov.duration)
     
         io.clearEvents()
-        tracker.setRecordingState(True)
+        if EYE_TRACKER:
+            tracker.setRecordingState(True)
         run_trial = True
 #        tstart_time = core.getTime()
         
@@ -105,12 +113,18 @@ for block in range(BLOCK_REPS):
         
         while (run_trial is True)&(mov.status != visual.FINISHED):
                 
-            # Get the latest gaze position in display coord space
-            gpos = tracker.getLastGazePosition()
+            # if tracker is on, test whether subject is fixating
+            if EYE_TRACKER:
+                # Get the latest gaze position in display coord space
+                gpos = tracker.getLastGazePosition()
         
-            # Update stim based on gaze position
-            valid_gaze_pos = isinstance(gpos, (tuple, list))
-            gaze_in_region = valid_gaze_pos and gaze_ok_region.contains(gpos)
+                # Update stim based on gaze position
+                valid_gaze_pos = isinstance(gpos, (tuple, list))
+                gaze_in_region = valid_gaze_pos and gaze_ok_region.contains(gpos)
+            else: # else ignore
+                valid_gaze_pos = True
+                gaze_in_region = True
+                
             if valid_gaze_pos:
                 # If we have a gaze position from the tracker, update gc stim
                 # and text stim
@@ -119,24 +133,27 @@ for block in range(BLOCK_REPS):
                     
                     # Only flip when a new frame should be displayed
                     if shouldflip:
-                        # Movie has already been drawn , so just draw text stim and flip
+                        # Movie has already been drawn, so just draw text stim and flip
                         text.draw()
                         win.flip()
                     else:
                         # Give the OS a break if a flip is not needed
                         time.sleep(0.001)
-                    # Drawn movie stim again
+                        
+                    # Draw movie stim again
                     shouldflip = mov.draw()
-                    
                     photodiode.draw()
                 else:
                     gaze_in_region = 'No'
                     mov.status = visual.FINISHED
                         
-                TRIAL_N = t + block * len(videolist)  
-                text_stim.text = text_stim_str % (gpos[0], gpos[1], gaze_in_region, TRIAL_N)
-        
-                gaze_dot.setPos(gpos)
+                TRIAL_N = t + block * len(videolist)
+                # Update text
+                if EYE_TRACKER:
+                    text_stim.text = text_stim_str % (gpos[0], gpos[1], gaze_in_region, TRIAL_N)
+                    gaze_dot.setPos(gpos)
+                else:
+                    text_stim.text = missing_gpos_str
             else:
                 # Otherwise just update text stim
                 text_stim.text = missing_gpos_str
@@ -145,6 +162,7 @@ for block in range(BLOCK_REPS):
             gaze_ok_region.draw()
             text_stim.draw()
             if valid_gaze_pos:
+                if EYE_TRACKER:
                 gaze_dot.draw()
         
             # Display updated stim on screen
@@ -163,15 +181,17 @@ for block in range(BLOCK_REPS):
         # Display updated stim on screen
         flip_time = win.flip(clearBuffer=True)
         
+        if EYE_TRACKER:
+            # Stop eye data recording
+            tracker.setRecordingState(False)
+        
+        t += 1        
         time.sleep(.5)
-                
-        # Stop eye data recording
-        tracker.setRecordingState(False)
-        t += 1
 
 # All Trials are done
 # End experiment
 win.close()
-tracker.setConnectionState(False)
+if EYE_TRACKER:
+    tracker.setConnectionState(False)
 io.quit()
 core.quit()
