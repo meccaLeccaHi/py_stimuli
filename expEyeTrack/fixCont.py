373d7ce3606974e388f92934408b49a205bc0652
adam <ajones173@gmail.com>
2017-01-24 17:32:34 -0600

    added generation of header info

--- expEyeTrack/fixCont.py
+++ expEyeTrack/fixCont.py
@@ -18,16 +18,31 @@ import numpy as np
 
 # Find movies matching wildcard search
 videopath = '/home/adam/Desktop/virtBox_share/JonesStimset/identity1/'
-videolist = glob.glob(videopath + '*100_audVid.avi')
+videolist = glob.glob(videopath + '*_audVid.avi')
 
 # Number of trials of each stimulus to run
-BLOCK_REPS = 1
+BLOCK_REPS = 2
+# Inter-stimulus interval (seconds)
+ISI = .5
+# Jitter range (+/-seconds)
+JITTER = .1
+# Scaling of image (none = 1)
+SCALE = 1
+# Total trial count for experiment
 TRIAL_COUNT = len(videolist) * BLOCK_REPS
 
-# Set-up screen
-SCREEN_SIZE = np.array([1280, 1024])
-PHOTO_SIZE = 100
-PHOTO_POS = (SCREEN_SIZE - PHOTO_SIZE)/2 * [1, -1]
+# Create new stimulus order
+new_order = np.tile(np.random.permutation(len(videolist)),BLOCK_REPS)
+# Re-order stimulus list
+videolist = [ videolist[i] for i in new_order]
+
+# Create jitter times
+jitter_times = np.random.uniform(-JITTER, JITTER, TRIAL_COUNT)
+
+# Create header
+
+# toggle for debugging mode
+TESTING = 1; # 1: yes, 0: no
 
 # toggle for presence of tracker
 EYE_TRACKER = 0; # 1: yes, 0: no
@@ -36,6 +51,17 @@ SIMULATE = 1; # 1: yes, 0: no
 
 # toggle for presence of joystick (N64 only, currently)
 JOYSTICK = 0; # 1: yes, 0: no
+# Set-up screen
+if TESTING:
+    FLSCRN = False
+    SCREEN_SIZE = np.array([280, 200])
+else:
+    FLSCRN = True
+    SCREEN_SIZE = np.array([1280, 1024]) 
+    
+# Set-up photodiode
+PHOTO_SIZE = 100
+PHOTO_POS = (SCREEN_SIZE - PHOTO_SIZE)/2 * [1, -1]
     
 if EYE_TRACKER:
     # Set-up tracker configuration dict
@@ -57,7 +83,7 @@ if EYE_TRACKER:
 else:
     io = launchHubServer()
   
-if JOYSTICK==0:
+if JOYSTICK:
     # Initialize the joystick
     pygame.init()
     pygame.joystick.init()    
@@ -74,7 +100,7 @@ if EYE_TRACKER:
     
 win = visual.Window(SCREEN_SIZE.tolist(),
                     units='pix',
-                    fullscr=True,
+                    fullscr=FLSCRN,
                     allowGUI=False,
                     color='black'
                     )
@@ -101,118 +127,114 @@ text_stim = visual.TextStim(win, text=text_stim_str,
                                  
 globalClock = core.Clock()  # to track the time since experiment started
 
-# Run blocks.....
-for block in range(BLOCK_REPS):
-             
-    t = 1         
-    # Run Trials.....
-    for vidPath in videolist:
+# Run Trials.....
+#for vidPath in videolist:
+for TRIAL_N in range(TRIAL_COUNT):
     
-        # Create False variable for joystick button        
-        button = 0;
+    # Create False variable for joystick button        
+    button = 0;
         
-        # Create movie stim by loading movie from list
-        mov = visual.MovieStim3(win, vidPath, size=(366, 332), fps = 30,
-                                flipVert=False, flipHoriz=False, loop=False) 
+    # Create movie stim by loading movie from list
+    mov = visual.MovieStim3(win, videolist[TRIAL_N], size=(366, 332), fps = 30,
+                            flipVert=False, flipHoriz=False, loop=False) 
     
-        io.clearEvents()
-        if EYE_TRACKER:
-            tracker.setRecordingState(True)
-        run_trial = True
-#        tstart_time = core.getTime()
+    io.clearEvents()
+    if EYE_TRACKER:
+        tracker.setRecordingState(True)
+    run_trial = True
+    tstart_time = core.getTime()
         
-        # Start the movie stim by preparing it to play
-        shouldflip = mov.play()
+    # Start the movie stim by preparing it to play
+    shouldflip = mov.play()
         
-        # Create crude frame counter
-        frame = 0;
+    # Create crude frame counter
+    frame = 0;
         
-        while (run_trial is True)&(mov.status != visual.FINISHED):
+    while (run_trial is True)&(mov.status != visual.FINISHED):
                 
-            # if tracker is on, test whether subject is fixating
-            if EYE_TRACKER:
-                # Get the latest gaze position in display coord space
-                gpos = tracker.getLastGazePosition()
+        # if tracker is on, test whether subject is fixating
+        if EYE_TRACKER:
+            # Get the latest gaze position in display coord space
+            gpos = tracker.getLastGazePosition()
         
-                # Update stim based on gaze position
-                valid_gaze_pos = isinstance(gpos, (tuple, list))
-                gaze_in_region = valid_gaze_pos and gaze_ok_region.contains(gpos)
-            else: # else ignore
-                valid_gaze_pos = True
-                gaze_in_region = True
+            # Update stim based on gaze position
+            valid_gaze_pos = isinstance(gpos, (tuple, list))
+            gaze_in_region = valid_gaze_pos and gaze_ok_region.contains(gpos)
+        else: # else ignore
+            valid_gaze_pos = True
+            gaze_in_region = True
                 
 
-            if valid_gaze_pos:
-                # If we have a gaze position from the tracker, update gc stim
-                # and text stim
-                if gaze_in_region:
-                    gaze_in_region = 'Yes'
+        if valid_gaze_pos:
+            # If we have a gaze position from the tracker, update gc stim
+            # and text stim
+            if gaze_in_region:
+                gaze_in_region = 'Yes'
                     
-                    # Only flip when a new frame should be displayed
-                    if shouldflip:
-                        # Movie has already been drawn, so just draw text stim and flip
-                        text.draw()
-                        win.flip()
-                    else:
-                        # Give the OS a break if a flip is not needed
-                        time.sleep(0.001)
+                # Only flip when a new frame should be displayed
+                if shouldflip:
+                    # Movie has already been drawn, so just draw text stim and flip
+                    text.draw()
+                    win.flip()
+                else:
+                    # Give the OS a break if a flip is not needed
+                    time.sleep(0.001)
                         
-                    # Draw movie stim again
-                    shouldflip = mov.draw()
-                    photodiode.draw()
+                # Draw movie stim again
+                shouldflip = mov.draw()
+                photodiode.draw()
                     
-                    # Increase frame number count
-                    frame += 1
-                else:
-                    gaze_in_region = 'No'
-                    mov.status = visual.FINISHED
-                        
-                TRIAL_N = t + block * len(videolist)
-                # Update text
-                if EYE_TRACKER:
-                    text_stim.text = text_stim_str % (gpos[0], gpos[1], gaze_in_region, TRIAL_N)
-                    gaze_dot.setPos(gpos)
-                else:
-                    text_stim.text = missing_gpos_str
-                            
+                # Increase frame number count
+                frame += 1
             else:
-                # Otherwise just update text stim
+                gaze_in_region = 'No'
+                mov.status = visual.FINISHED
+                        
+            #TRIAL_N = t + block * len(videolist)
+            # Update text
+            if EYE_TRACKER:
+                text_stim.text = text_stim_str % (gpos[0], gpos[1], gaze_in_region, TRIAL_N)
+                gaze_dot.setPos(gpos)
+            else:
                 text_stim.text = missing_gpos_str
+                            
+        else:
+            # Otherwise just update text stim
+            text_stim.text = missing_gpos_str
         
-            # Redraw stim
-            gaze_ok_region.draw()
-            text_stim.draw()
-            if valid_gaze_pos:
-                if EYE_TRACKER:
-                    gaze_dot.draw()
-        
-            # Display updated stim on screen
-            flip_time = win.flip()
-            
-            # Check joystick for trigger press
-            if JOYSTICK and frame%5==0:
-                pygame.event.poll() # Look for joystick events
-                button = joystick.get_button( 7 ) # 'Z'-trigger button
-        
-            # Check any new keyboard char events for a space key
-            # If one is found, set the trial end variable
-            if ' ' in keyboard.getPresses() or mov.status == visual.FINISHED or button:
-                run_trial = False
-    
-        # Current Trial is Done
-    
         # Redraw stim
         gaze_ok_region.draw()
         text_stim.draw()
+        if valid_gaze_pos:
+            if EYE_TRACKER:
+                gaze_dot.draw()
+        
         # Display updated stim on screen
-        flip_time = win.flip(clearBuffer=True)
+        flip_time = win.flip()
+            
+        # Check joystick for trigger press
+        if JOYSTICK and frame%5==0:
+            pygame.event.poll() # Look for joystick events
+            button = joystick.get_button( 7 ) # 'Z'-trigger button
         
-        if EYE_TRACKER:
-            # Stop eye data recording
-            tracker.setRecordingState(False)
+        # Check any new keyboard char events for a space key
+        # If one is found, set the trial end variable
+        if ' ' in keyboard.getPresses() or mov.status == visual.FINISHED or button:
+            run_trial = False
+    
+    # Current Trial is Done
+    
+    # Redraw stim
+    gaze_ok_region.draw()
+    text_stim.draw()
+    # Display updated stim on screen
+    flip_time = win.flip(clearBuffer=True)
         
-        t += 1        
-        time.sleep(.5)
+    if EYE_TRACKER:
+        # Stop eye data recording
+        tracker.setRecordingState(False)
+        
+    time.sleep(ISI + jitter_times[TRIAL_N])
 
 # All Trials are done
 # End experiment
